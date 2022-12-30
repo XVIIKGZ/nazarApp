0370f7ed02b1efeccc6cb4639dee35f74d3008c4
XVIIKGZ <nazar.kydyraliev@gmail.com>
2022-12-30 21:00:29 +0600

    Initial Commit

---
+++ m2h21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  m2h21
+//
+//  Created by Nazar Kydyraliev on 30/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
